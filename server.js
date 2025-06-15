53dea7bc21174f5441bae82033d0853205e1068a
sobanamjad49 <sobanamjad49@gmail.com>
2025-06-15 19:34:15 +0500

    add static url

--- server.js
+++ server.js
@@ -22,21 +22,45 @@ app.use("/products", productRoute); // 🛍️ Product management
 app.use("/orders", orderRoute); // 📦 Orders
 app.use("/cart", cartRoute); // 🛒 Cart
 
-// ✅ MongoDB Connection
-mongoose
-  .connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("✅ MongoDB connected successfully"))
-  .catch((err) => console.error("❌ MongoDB connection failed:", err));
 
-app.get("/", (req, res) => {
-  res.send("Hello from railway + Express!");
-});
+async function startServer() {
+  try {
+    await mongoose.connect("mongodb+srv://soban663434:soban663434@cluster0.gww34n9.mongodb.net/sample_mflix?retryWrites=true&w=majority&appName=Cluster0", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("✅ MongoDB connected");
 
-// ✅ Start Express Server
-const PORT = process.env.PORT || 7474;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
-});
+    app.get("/", (req, res) => {
+      res.send("Hello from railway + Express!");
+    });
+
+    const PORT = process.env.PORT || 7474;
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running at http://localhost:${PORT}`);
+    });
+
+  } catch (err) {
+    console.error("❌ MongoDB connection failed:", err);
+  }
+}
+
+startServer();
+// // ✅ MongoDB Connection
+// mongoose
+//   .connect("mongodb+srv://soban663434:soban663434@cluster0.gww34n9.mongodb.net/outfitter?retryWrites=true&w=majority", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log("✅ MongoDB connected successfully"))
+//   .catch((err) => console.error("❌ MongoDB connection failed:", err));
+
+// app.get("/", (req, res) => {
+//   res.send("Hello from railway + Express!");
+// });
+
+// // ✅ Start Express Server
+// const PORT = process.env.PORT || 7474;
+// app.listen(PORT, () => {
+//   console.log(`🚀 Server running at http://localhost:${PORT}`);
+// });
